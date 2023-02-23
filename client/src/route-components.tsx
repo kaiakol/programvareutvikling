9b2cb2ab79cbffd217857157a25acc6380adb9f9
Kaia Kolstad <kaiako@stud.ntnu.no>
2023-02-23 12:47:10 +0100

    #12 Making design for the explore page

--- client/src/route-components.tsx
+++ client/src/route-components.tsx
@@ -3,7 +3,7 @@ import { Component } from "react-simplified";
 import { NavLink } from "react-router-dom";
 import routeService, { Route } from "./route-service";
 import { createHashHistory } from "history";
-import { Card, Row, Col, Container } from "react-bootstrap";
+import { Card, Row, Col, Container, Stack } from "react-bootstrap";
 
 const history = createHashHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
 
@@ -16,22 +16,65 @@ export class RouteList extends Component {
   render() {
     const sortedRoutes = this.routes.sort((a, b) => a.route_id - b.route_id);
     // Render each group on a separate row
+    const groupedRoutes = this.routes.reduce(
+      (acc: { [key: number]: Route[] }, curr: Route) => {
+        if (acc[curr.route_id]) {
+          acc[curr.route_id].push(curr);
+        } else {
+          acc[curr.route_id] = [curr];
+        }
+        return acc;
+      },
+      {}
+    );
     return (
       <>
         <Container>
-          <Card.Title>Routes</Card.Title>
-
-          {sortedRoutes.map((route) => (
-            <Row key={route.route_id}>
-              <Col>
-                <NavLink to={"/routes/" + route.route_id}>
-                  {route.destination}, {route.route_id}
+          {Object.keys(groupedRoutes).map((routeId) => {
+            const routes = groupedRoutes[routeId];
+            return (
+              <Card key={routeId}>
+                <NavLink
+                  to={"/routes/" + routeId}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Card.Title>Route</Card.Title>
                 </NavLink>
-              </Col>
-            </Row>
-          ))}
+                <Row>
+                  <Col>Destinations:</Col>
+                  <Col>
+                    {routes.map((route, index) => (
+                      <span key={index}>
+                        {route.destination}
+                        {index === routes.length - 1 ? "" : ", "}
+                      </span>
+                    ))}
+                  </Col>
+                </Row>
+              </Card>
+            );
+          })}
         </Container>
       </>
+
+      // <Container>
+      //   <Card.Title>Routes</Card.Title>
+      //   <Stack direction="horizontal" gap={3}>
+      //     {sortedRoutes.map((route) => (
+      //       <Row key={route.route_id}>
+      //         <Col>
+      //           <Card>
+      //             <Col>
+      //               <NavLink to={"/routes/" + route.route_id}>
+      //                 {route.destination}, {route.route_id}
+      //               </NavLink>
+      //             </Col>
+      //           </Card>
+      //         </Col>
+      //       </Row>
+      //     ))}
+      //   </Stack>
+      // </Container>
     );
   }
 
